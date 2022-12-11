506ac09b733e0627d82eca95b1f9953f19b7064c
NonPeritum <77570814+JVormstein@users.noreply.github.com>
2022-12-11 13:34:05 +0100

    Initial commit

---
+++ psp/__init__.py
@@ -0,0 +1,11 @@
+'''
+    This file is PSP's __init__ file (See Python docs for more information).
+    It contains all functions required for running PSP as a standalone.
+'''
+
+
+def start_cli():
+    '''
+    Starts the CLI
+    '''
+    
